7d01770341ce11d30c6d448b79fbe417701ff8de
EliavC <eliavc2001@gmail.com>
2025-01-23 15:13:37 +0200

    mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
+import initApp from './server'
 import app from './server'
 
 const port = process.env.PORT
 
-app.listen(port,()=>{
-    console.log("server is on port 3000")
+initApp().then((app)=>{
+    app.listen(port,()=>{
+        console.log(`server is on port ${port}`)
+    });
 })
+// app.listen(port,()=>{
+//     console.log("server is on port 3000")
+// })
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,31 @@
 import express, {Express} from "express"
 const app = express()
-
 import dotenv  from "dotenv" 
 dotenv.config();
-
+import mongoose from "mongoose";
 import bodyParser from "body-parser";
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-export default app
+const initApp = ()=>{
+    return new Promise<Express>((resolve,reject)=>{
+    
+        const db=mongoose.connection
+        db.on('error',error=>{console.error(error)})
+        db.once('open',()=>console.log('connected to mongo'))
+    
+        if(process.env.DATABASE_URL === undefined)
+        {
+            console.error("DATABASE_URL isn't set")
+            reject()
+        }
+        else{
+            mongoose.connect(process.env.DATABASE_URL).then(()=>{
+                resolve(app)
+            })
+        }
+        
+        })
+    }
+
+export default initApp
